956d9001c6e481a5e65a7f70579585755fd45a8a
awm.builds <81087554+awm-builds@users.noreply.github.com>
2024-06-04 17:11:12 -0400

    We added  urls, views, settings, base.html, home.html

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# View functions pull from models
+# Define the home view
+def home(request):
+  # Include an .html file extension - unlike when rendering EJS templates
+  return render(request, 'home.html')
